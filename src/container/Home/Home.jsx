ad81ce9c0e7fd6701e0be1d7d1c66fd92b0abac1
Yoga <yogasedana63@gmail.com>
2022-10-13 14:11:19 +0800

    mengingat materi yang pernah di pelajari

--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -1,32 +1,55 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import YouTubeComp from "../../component/YoutubeComp/YouTubeComp";
 
 class Home extends Component {
-    render () {
-        return (
-            <div>
-                <p>Youtube Component</p>
-                <hr />
-                <YouTubeComp 
-                time="7.21" 
-                title="Langkah 1" 
-                desc="Yoga Sedana (RYGH) . 50 x ditonton . 1 hari yang lalu"/>
-                <YouTubeComp 
-                time="10.11" 
-                title="Langkah 2" 
-                desc="Yoga Sedana (RYGH) . 100 x ditonton . 2 hari yang lalu"/>
-                <YouTubeComp 
-                time="9.01" 
-                title="Langkah 3" 
-                desc="Yoga Sedana (RYGH) . 6 x ditonton . 3 hari yang lalu"/>
-                <YouTubeComp 
-                time="8.20" 
-                title="Langkah 4" 
-                desc="Yoga Sedana (RYGH) . 510 x ditonton . 4 hari yang lalu"/>
-                <YouTubeComp />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <p>Youtube Component</p>
+        <hr />
+        <YouTubeComp
+          time="7.21"
+          title="Langkah 1"
+          desc="Yoga Sedana (RYGH) . 50 x ditonton . 1 hari yang lalu"
+        />
+        <YouTubeComp
+          time="10.11"
+          title="Langkah 2"
+          desc="Yoga Sedana (RYGH) . 100 x ditonton . 2 hari yang lalu"
+        />
+        <YouTubeComp
+          time="9.01"
+          title="Langkah 3"
+          desc="Yoga Sedana (RYGH) . 6 x ditonton . 3 hari yang lalu"
+        />
+        <YouTubeComp
+          time="8.20"
+          title="Langkah 4"
+          desc="Yoga Sedana (RYGH) . 700 x ditonton . 4 hari yang lalu"
+        />
+        <YouTubeComp
+          ime="8.20"
+          title="Langkah 4"
+          desc="Yoga Sedana (RYGH) . 510 x ditonton . 4 hari yang lalu"
+        />
+        <YouTubeComp
+          ime="8.20"
+          title="Langkah 4"
+          desc="Yoga Sedana (RYGH) . 5110 x ditonton . 4 hari yang lalu"
+        />
+        <YouTubeComp
+          ime="8.20"
+          title="Langkah 4"
+          desc="Yoga Sedana (RYGH) . 5000 x ditonton . 4 hari yang lalu"
+        />
+        <YouTubeComp
+          ime="8.20"
+          title="Langkah 4"
+          desc="Yoga Sedana (RYGH) . 510 x ditonton . 4 hari yang lalu"
+        />
+      </div>
+    );
+  }
 }
 
 export default Home;
